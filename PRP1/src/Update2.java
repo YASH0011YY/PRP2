40f8c743f5d59c2a9cac743deeb1bbaa762d883a
sdac15 <sdac15@sdac15-PC>
2023-06-14 15:00:43 +0530

    Update 2.0

--- PRP1/src/Update2.java
+++ PRP1/src/Update2.java
@@ -1,9 +1,9 @@
 
-public class Update1 {
+public class Update2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("UPDATE 1.0");
+		System.out.println("UPDATE 2.0");
 	}
 
 }
